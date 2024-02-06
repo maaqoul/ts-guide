efbe18dd68974c0b13c27ef33e289644ebb6bfe5
iamara <iamara@sqli.com>
2024-02-06 18:07:20 +0100

    TypeScript Exercises

--- IbrahimAmara/hello.ts
+++ IbrahimAmara/hello.ts
@@ -102,3 +102,141 @@ const t: Circle = {
 const movedCircle = moveCircle(t, 3, 4);
 console.log(movedCircle); // console: { centerX: 3, centerY: 4, radius: 2 }
 console.log(t); // console: { centerX: 0, centerY: 1, radius: 2 }
+
+
+// exercice 9
+
+class Animal {
+    name: string;
+    constructor(theName: string) { this.name = theName; }
+    move(distanceInMeters: number = 0) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+class Dog extends Animal {
+    constructor(name: string) {
+        super(name);
+    }
+
+    move(distanceInMeters: number) {
+        console.log(`${this.name} dog moved ${distanceInMeters}m `);
+    }
+}
+
+let dog=new Dog('messi');
+let animal=new Animal('chat');
+
+dog.move(5); //console messi  dog moved 5m
+animal.move(10); //console chat moved 10m.
+
+//exercice 10
+
+class Person {
+    private name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }
+}
+
+let per=new Person('ibrahim');
+//console.log(per.name); // console error Property 'name' is private and only accessible within class 'Person'.
+console.log(per.getName()); //console : ibrahim
+
+
+//exercice 11
+
+function identity<T>(arg: T): T {
+    console.log(`Type of arg : ${typeof arg}`);
+    return arg;
+}
+
+const testSting : string=identity("test"); // console Type of arg : string
+console.log(testSting); // console test
+const testInt : number=identity(8); // console Type of arg : number
+console.log(testInt); // console 8
+const testBool : boolean=identity(true); // console Type of arg : boolean
+console.log(testBool); // console true
+const testArrayString : string[] =identity(['lundi', 'mardi']); // console Type of arg : object
+console.log(testArrayString); // console [ 'lundi', 'mardi' ]
+const testArrayNumber : number[] =identity([0, 1,2,3]); // console Type of arg : object
+console.log(testArrayNumber); // console  [ 0, 1, 2, 3 ]
+const testAny : any =identity(['test1', 1,'2',3]); // console Type of arg : object
+console.log(testAny); //  console [ 'test1', 1, '2', 3 ]
+
+
+// exercice 12
+
+interface GenericIdentityFn<T> {
+    (arg: T): T;
+}
+
+// Implémentation
+const implementString: GenericIdentityFn<string> = function<T>(arg: T): T {
+    return arg;
+};
+const implementNumber: GenericIdentityFn<number> = function<T>(arg: T): T {
+    return arg;
+};
+
+const testString = implementString("salam");
+console.log(testString); // console  : Salam
+const testNumber = implementNumber(55555);
+console.log(testNumber); // console  : 55555
+
+
+interface GenericArray<T> {
+    [index: number]: T;
+    length: number;
+}
+
+const testArray: GenericArray<number> = [1, 2, 3];
+
+console.log(testArray[0]); // console: 1
+console.log(testArray.length); // console: 3
+
+// exercice 13
+
+// @ts-ignore
+enum Response {
+    Non = 0,
+    Yes = 1,
+}
+
+function testResponse(response: Response): string {
+    return response === Response.Yes ? "Response YES" : "Response NO";
+}
+
+console.log(testResponse(Response.Yes)); // console Response YES
+console.log(testResponse(Response.Non)); // Response NO
+
+// exercice 14
+enum Message {
+    Success = "SUCCESS",
+    Failure = "FAILURE"
+}
+
+function testResult(status: boolean): Message {
+    return status ? Message.Success : Message.Failure;
+}
+
+console.log(testResult(true)); // console: SUCCESS
+console.log(testResult(false)); // console: FAILURE
+
+
+//exercice 15
+
+function padLeft(value: string, padding: string | number) {
+    return padding + value;
+}
+
+console.log(padLeft("salam", " ")); // Sortie: " salam"
+console.log(padLeft("salam", 4)); // Sortie: "4World"
+
+
+//exercice 16
+
+
